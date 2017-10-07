02476ebf0bcc5d31912a97ff4583d5654a815135
Doğancan Çolak <dogancancolak1@gmail.com>
2017-10-07 13:20:49 +0300

    User Model Created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstName = models.TextField(max_length=255)
+    lastName = models.TextField(max_length=255)
+    rewardCount = models.IntegerField(default=0)
+    rewardProgress = models.IntegerField(default=0)
+    surveyCount = models.IntegerField(default=0)
+
+    def getRewardProgress(self):
+        return self.rewardCount
+
+    def giveReward(self):
+        self.rewardProgress = 0
+        self.rewardCount = self.rewardCount+1
